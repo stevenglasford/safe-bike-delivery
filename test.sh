610610c9f1788cabca24a0dff463313c44100f90
Steven Glasford <stevenglasford@duck.com>
2023-10-23 11:08:21 -0500

    Try executing the distance formula

---
+++ test.sh
@@ -0,0 +1,8 @@
+curl -X POST https://cyclers.tech/v5/routing \
+-H "Content-Type: application/json" \
+-d '{
+  "client": "IOS",
+  "origin": {"lat": 50.105827, "lon": 14.415478},
+  "destination": {"lat": 50.090438, "lon": 14.368958},
+  "key": "ZK7hRQamGXpAeQDfRiCveVyBjdtGp7JU"
+}'
